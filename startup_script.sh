01bf0fa80bb752b5373acc32c764b4254db5616a
Chris Mague <github@mague.com>
2025-07-15 13:58:44 -0700

    update startup script to include golang

--- startup_script.sh
+++ startup_script.sh
@@ -1,7 +1,12 @@
 apt-get update
-apt-get install -y python3-pip python3-venv
+apt-get install -y python3-pip python3-venv golang-1.23
+
+cd /tmp
+/usr/bin/curl -L https://github.com/GoogleCloudPlatform/grpc-gcp-tools/releases/latest/download/dp_check -o dp_check
+chmod +x dp_check
 
 echo "# Set up environment variables" >> /etc/bash.bashrc
 echo "export GCP_PROJECT=${projectid}" >> /etc/bash.bashrc
 echo "export GCP_SPANNER_INSTANCE=${spannerinstance}" >> /etc/bash.bashrc
 echo "export GCP_SPANNER_DATABASE=${spannerdatabase}" >> /etc/bash.bashrc
+
