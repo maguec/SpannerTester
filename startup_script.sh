938bcf715a6842760ca9a06c218e89d9d0200177
Chris Mague <github@mague.com>
2025-07-15 13:26:04 -0700

    install python

--- startup_script.sh
+++ startup_script.sh
@@ -1,5 +1,5 @@
 apt-get update
-apt-get install -y python3-pip
+apt-get install -y python3-pip python3-venv
 
 echo "# Set up environment variables" >> /etc/bash.bashrc
 echo "export GCP_PROJECT=${projectid}" >> /etc/bash.bashrc
