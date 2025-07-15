4c6b5b672de9811747e61066a87f5f3fb0016cbe
Chris Mague <github@mague.com>
2025-07-15 16:39:12 -0700

    link binary in path

--- startup_script.sh
+++ startup_script.sh
@@ -5,6 +5,7 @@ cd /tmp
 /usr/bin/curl -L https://github.com/GoogleCloudPlatform/grpc-gcp-tools/releases/latest/download/dp_check -o dp_check
 chmod +x dp_check
 
+ln -s /usr/lib/go-1.23/bin/go /usr/bin/go
 echo "# Set up environment variables" >> /etc/bash.bashrc
 echo "export GCP_PROJECT=${projectid}" >> /etc/bash.bashrc
 echo "export GCP_SPANNER_INSTANCE=${spannerinstance}" >> /etc/bash.bashrc
